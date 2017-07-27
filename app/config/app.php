89f95d8dd2f58885259bb11c32db19510c629e63
Wanderson <wanderson.ferreira@webcorpore.com>
2017-07-27 20:20:22 -0300

    add timezone em config

--- app/config/app.php
+++ app/config/app.php
@@ -6,6 +6,10 @@ $config = [
 ];
 
 
+/* Configurações do PHP */
+ini_set('date.timezone', 'America/Sao_Paulo');
+
+
 /* Recaptcha */
 Win\Widget\ReCaptcha::$siteKey = '6LcDAioUAAAAAIMAHCFz02fuq7at3C6gf9_DIGum';
 Win\Widget\ReCaptcha::$secretKey = '6LcDAioUAAAAAKLXofatfq3FP2TLkgkIQSbJwto0';
