c458ac0bd6ad174fd84e5201271fc898de6ae324
Wanderson Ferreira <wanderson.ferreira@webcorpore.com>
2018-08-20 19:55:43 -0300

    scrutinizer tips

--- www/app/config/app.php
+++ www/app/config/app.php
@@ -4,17 +4,7 @@
 define('BASE_PATH', getcwd());
 
 
-/* Configurações básicas da aplicação */
-$config = [
-	'name' => 'winPHP Framework'
-];
-
 
 /* Configurações do PHP */
 ini_set('date.timezone', 'America/Sao_Paulo');
 
-
-
-/* Recaptcha */
-\Win\Html\Form\ReCaptcha::$siteKey = '6LcDAioUAAAAAIMAHCFz02fuq7at3C6gf9_DIGum';
-\Win\Html\Form\ReCaptcha::$secretKey = '6LcDAioUAAAAAKLXofatfq3FP2TLkgkIQSbJwto0';
