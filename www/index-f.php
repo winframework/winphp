3dfcbebd0201c986cbd66173643ec92c5ea17334
Wanderson Ferreira <wanderson.ferreira@webcorpore.com>
2018-08-20 20:27:04 -0300

    scrutinizer tips

--- www/index-f.php
+++ www/index-f.php
@@ -8,7 +8,7 @@
  * @link       http://fuelphp.com
  */
 require '../vendor/autoload.php';
-use Fuel\Foundation\Application;
+
 /**
  * Set error reporting and display errors settings.
  * You may want to change these when in production.
@@ -28,25 +28,3 @@ define('DOCROOT', __DIR__ . DIRECTORY_SEPARATOR);
  */
 define('VENDORPATH', realpath(__DIR__ . '/../vendor/') . DIRECTORY_SEPARATOR);
 define('DS', DIRECTORY_SEPARATOR);
-// Get the start time and memory for use later
-$startTime = microtime(true);
-$startMem = memory_get_usage();
-$app = Application::init([
-	'components' => [
-		'Fuel\Demo',
-	],
-]);
-$response = $app->run();
-$responseBody = (string) $response->getBody();
-if (strpos($responseBody, '{exec_time}') !== false or strpos($responseBody, '{mem_usage}') !== false)
-{
-	$time = microtime(true) - $startTime;
-	$mem = memory_get_usage() - $startMem;
-	$responseBody = str_replace(
-		array('{exec_time}', '{mem_usage}'),
-		array(round($time, 4), round($mem / pow(1024, 2), 3)),
-		$responseBody
-	);
-}
-http_response_code($response->getStatusCode());
-echo $responseBody;
