73eeb0dbde78cebcb6dca9ab31394e0e616d1391
Wanderson Ferreira <wanderson.ferreira@webcorpore.com>
2018-08-20 19:51:50 -0300

    scrutinizer tips

--- www/app/config/app.php
+++ www/app/config/app.php
@@ -12,3 +12,9 @@ $config = [
 
 /* Configurações do PHP */
 ini_set('date.timezone', 'America/Sao_Paulo');
+
+
+
+/* Recaptcha */
+\Win\Html\Form\ReCaptcha::$siteKey = '6LcDAioUAAAAAIMAHCFz02fuq7at3C6gf9_DIGum';
+\Win\Html\Form\ReCaptcha::$secretKey = '6LcDAioUAAAAAKLXofatfq3FP2TLkgkIQSbJwto0';
