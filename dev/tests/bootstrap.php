6878bffc403f5292ed74e51703cada850e3dddbf
Wanderson Ferreira <wanderson.ferreira@webcorpore.com>
2018-08-23 19:37:32 -0300

    fix tests/boostrap

--- dev/tests/bootstrap.php
+++ dev/tests/bootstrap.php
@@ -1,8 +1,6 @@
 <?php
 
-error_reporting(-1);
-
-define('BASE_PATH', getcwd());
+define('BASE_PATH', __DIR__);
 include BASE_PATH . '/autoload.php';
 include BASE_PATH . '/../../www/lib/functions/strings.php';
 
