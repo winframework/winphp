3a8c64e86b120aacbef921a95d7943a985e95a42
Wanderson Ferreira <wandersonbferreira@gmail.com>
2020-06-14 09:29:12 -0300

    fix email test

--- tests/phpunit/integration/Win/Common/EmailTest.php
+++ tests/phpunit/integration/Win/Common/EmailTest.php
@@ -13,9 +13,9 @@ class EmailTest extends TestCase
 	{
 		new Application();
 		$email = new Email(static::VALID, []);
-		$this->assertStringContainsString('id="email-body"', (string) $email);
+		$this->assertStringContainsString('id="email-body"', $email);
 
 		$email = new Email(static::VALID, [], 'layout-not-found');
-		$this->assertStringContainsString('id="email-body"', (string) $email);
+		$this->assertStringNotContainsString('id="email-body"', (string) $email);
 	}
 }
