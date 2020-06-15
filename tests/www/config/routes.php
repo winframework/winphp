03cc538e2a1f765f3cab8373c29c5c6064dd73db
Wanderson Ferreira <wandersonbferreira@gmail.com>
2020-06-15 20:59:02 -0300

    fix comments

--- tests/www/config/routes.php
+++ tests/www/config/routes.php
@@ -3,9 +3,9 @@
 use Win\Request\Router;
 
 /**
- * Direciona uma URL para um Controller/action
+ * Direciona uma URL para um Controller@action
  *
- * url => NomeDoController/nomeDoAction
+ * url => NomeDoController@nomeDoAction
  */
 Router::add('App\\Controllers\\', [
 	'' => 'IndexController@index',
