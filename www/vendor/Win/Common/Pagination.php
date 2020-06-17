38fb20698f5292f288329df1e4fb2de436b916c6
Wanderson Ferreira <wandersonbferreira@gmail.com>
2020-06-16 23:10:16 -0300

    Update Pagination.php

--- www/vendor/Win/Common/Pagination.php
+++ www/vendor/Win/Common/Pagination.php
@@ -10,8 +10,6 @@ class Pagination
 	public int $current;
 	public int $pageSize;
 	public int $count = 0;
-	public int $prev;
-	public int $next;
 	public int $last;
 	public int $offset;
 
@@ -30,8 +28,6 @@ class Pagination
 		$this->count = $count;
 		$this->last = ceil($count / $this->pageSize);
 		$this->current = min($this->last, $this->current);
-		$this->prev = max(1, $this->current - 1);
-		$this->next = min($this->last, $this->current + 1);
 		$this->offset = $this->pageSize * ($this->current - 1);
 	}
 }
