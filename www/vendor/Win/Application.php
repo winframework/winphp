a8f440d4dfa279895b0037a321b419defec4641b
Wanderson Ferreira <wandersonbferreira@gmail.com>
2020-10-12 12:50:27 -0300

    fix application

--- www/vendor/Win/Application.php
+++ www/vendor/Win/Application.php
@@ -16,7 +16,7 @@ use Win\Templates\View;
  *
  * Framework em PHP baseado em MVC
  * Responsável por incluir as páginas de acordo com a URL e criar a estrutura MVC
- * @author winPHP Framework <http://github.com/winframework/winphp/>
+ * @author winPHP Framework http://github.com/winframework/winphp/
  * @version 1.7.0
  */
 class Application
