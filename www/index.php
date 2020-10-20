144fd0bb39262e46de67c63f9946acceb31a16a1
Wanderson Ferreira <wandersonbferreira@gmail.com>
2020-10-20 18:11:46 -0300

    update index

--- www/index.php
+++ www/index.php
@@ -11,15 +11,14 @@ use Win\Services\Router;
 
 define('BASE_PATH', __DIR__);
 require 'app/autoload.php';
-require 'config/app.php';
-require 'config/routes.php';
 
 try {
+	require 'config/app.php';
+	require 'config/routes.php';
 	$app = new Application();
 	$app->run(...Router::instance()->getDestination());
 } catch (HttpException $e) {
 	$app->run(ErrorsController::class, "_{$e->getCode()}", $e);
 } catch (Throwable $e) {
-	ob_clean();
 	$app->run(ErrorsController::class, '_500', $e);
 }
